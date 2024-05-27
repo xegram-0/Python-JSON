2ea7064f523ec2e4990ef2ea8169b13c8985fa3a
Hong Nhan <nguyenhongnhan9a5@gmail.com>
2024-05-27 22:51:07 +0700

    Solved 1

--- Exercise 1: Convert the following dictionary into JSON format.py
+++ Exercise 1: Convert the following dictionary into JSON format.py
@@ -1,1 +1,13 @@
 # Exercise 1: Convert the following dictionary into JSON format
+# data = {"key1" : "value1", "key2" : "value2"}
+
+# Expected Output:
+
+# data = {"key1" : "value1", "key2" : "value2"}
+
+import json
+
+data = {"key1" : "value1", "key2" : "value2"}
+print(f"Converting {data} into JSON format...")
+json_data = json.dumps(data)
+print(json_data)
