60f797f338fa8b3d037108319ec4a9af4fa41c9b
Hong Nhan <nguyenhongnhan9a5@gmail.com>
2024-05-27 22:56:13 +0700

    Solved 2

--- Exercise 2: Access the value of key2 from the following JSON.py
+++ Exercise 2: Access the value of key2 from the following JSON.py
@@ -1,1 +1,15 @@
 # Exercise 2: Access the value of key2 from the following JSON
+# import json
+
+# sampleJson = """{"key1": "value1", "key2": "value2"}"""
+# # write code to print the value of key2
+
+# Expected Output:
+
+# value2
+
+import json
+
+sampleJson = """{"key1": "value1", "key2": "value2"}"""
+json_data = json.loads(sampleJson)
+print(json_data["key2"])
