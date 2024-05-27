96a76ad54707ee21d5ffe91ec4d04bbb6411157e
Hong Nhan <nguyenhongnhan9a5@gmail.com>
2024-05-27 23:03:15 +0700

    Solved 3

--- Exercise 3: PrettyPrint following JSON data.py
+++ Exercise 3: PrettyPrint following JSON data.py
@@ -1,1 +1,23 @@
 # Exercise 3: PrettyPrint following JSON data
+# PrettyPrint following JSON data with indent level 2 and key-value separators should be (",", " = ").
+
+# sampleJson = {"key1": "value1", "key2": "value2"}
+
+# Expected Output:
+
+# {
+#   "key1" = "value2",
+#   "key2" = "value2",
+#   "key3" = "value3"
+# }
+
+import json
+
+sampleJson = {"key1": "value1", "key2": "value2"}
+
+# The samplejson is not a true json kind (with """""")
+# thus resulting in error
+#json_data = json.loads(sampleJson)
+
+pretty_json = json.dumps(sampleJson, indent=2, separators=(",", " = "))
+print(pretty_json)
