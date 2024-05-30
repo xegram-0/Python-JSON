a7a34444e5657c949aa6604b17cf841229f7c2ad
Hong Nhan <nguyenhongnhan9a5@gmail.com>
2024-05-30 16:35:51 +0700

    Solved 6

--- Exercise 6: Convert the following Vehicle Object into JSON.py
+++ Exercise 6: Convert the following Vehicle Object into JSON.py
@@ -20,17 +20,21 @@
 # }
 
 import json
-
+from json import JSONEncoder
 class Vehicle:
     def __init__(self, name, engine, price):
         self.name = name
         self.engine = engine
         self.price = price
 
+class VehicleEncoder(JSONEncoder):
+    def default(self, o):
+        return o.__dict__
+    
 vehicle = Vehicle("Toyota Rav4", "2.5L", 32000)
 
-obj_to_json = [f"{vehicle.name},{vehicle.engine},{vehicle.price}"]
+#obj_to_json = [f"{vehicle.name},{vehicle.engine},{vehicle.price}"]
 #print(obj_to_json)
-json_data = json.dumps(obj_to_json,indent=4,cls=)
+json_data = json.dumps(vehicle,indent=4,cls=VehicleEncoder)
 print(json_data)
 
