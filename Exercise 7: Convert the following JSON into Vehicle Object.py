eb547d4df210189cb56e4cb1e4394dd9883cc6f4
Hong Nhan <nguyenhongnhan9a5@gmail.com>
2024-05-30 16:37:08 +0700

    Added description

--- Exercise 7: Convert the following JSON into Vehicle Object.py
+++ Exercise 7: Convert the following JSON into Vehicle Object.py
@@ -1,1 +1,8 @@
 # Exercise 7: Convert the following JSON into Vehicle Object
+# { "name": "Toyota Rav4", "engine": "2.5L", "price": 32000 }
+
+# For example, we should be able to access Vehicle Object using the dot operator like this.
+
+# vehicleObj.name, vehicleObj.engine, vehicleObj.price
+
+
