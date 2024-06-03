c0819faae9f0f225b0da03d95b7fcce4abad25a8
Hong <nguyenhongnhan9a5@gmail.com>
2024-06-03 16:25:21 +0700

    Solved 8

--- Exercise 8: Check whether following json is valid or invalid. If Invalid correct it.py
+++ Exercise 8: Check whether following json is valid or invalid. If Invalid correct it.py
@@ -1,3 +1,39 @@
 # Exercise 8: Check whether following json is valid or invalid. If Invalid correct it
 
-# debt 1
+# { 
+#    "company":{ 
+#       "employee":{ 
+#          "name":"emma",
+#          "payble":{ 
+#             "salary":7000
+#             "bonus":800
+#          }
+#       }
+#    }
+# }
+
+import json
+
+def validateJson(data):
+    try:
+        json.loads(data)
+    except ValueError as error:
+        return "invalid"
+    return "valid"
+
+# The data must be in string ("""
+#                               """)
+json_data = """{ 
+   "company":{ 
+      "employee":{ 
+         "name":"emma",
+         "payble":{ 
+            "salary":7000,
+            "bonus":800
+         }
+      }
+   }
+}
+"""
+result = validateJson(json_data)
+print(f"The json data is {result}")
