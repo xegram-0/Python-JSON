8879a290edd65dbe6cdab758becc547bfccce356
Hong <nguyenhongnhan9a5@gmail.com>
2024-06-03 16:36:54 +0700

    Solved 9

--- Exercise 9: Parse the following JSON to get all the values of a key ‘name’ within an array.py
+++ Exercise 9: Parse the following JSON to get all the values of a key ‘name’ within an array.py
@@ -1,3 +1,53 @@
 # Exercise 9: Parse the following JSON to get all the values of a key ‘name’ within an array
-#debt 2
-#debt 3 out of power no wifi
+
+
+# [ 
+#    { 
+#       "id":1,
+#       "name":"name1",
+#       "color":[ 
+#          "red",
+#          "green"
+#       ]
+#    },
+#    { 
+#       "id":2,
+#       "name":"name2",
+#       "color":[ 
+#          "pink",
+#          "yellow"
+#       ]
+#    }
+# ]
+# Expected Output:
+
+# ["name1", "name2"]
+
+
+import json 
+
+json_data = """
+[ 
+   { 
+      "id":1,
+      "name":"name1",
+      "color":[ 
+         "red",
+         "green"
+      ]
+   },
+   { 
+      "id":2,
+      "name":"name2",
+      "color":[ 
+         "pink",
+         "yellow"
+      ]
+   }
+]
+"""
+
+
+the_data = json.loads(json_data)
+targetData = [item.get("name") for item in the_data]
+print(targetData)
